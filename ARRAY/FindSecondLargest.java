89bfaabb619744b96f7def4a73b885573e0017e2
soumyabiswal <biswalsoumya342@gmail.com>
2025-06-19 11:40:57 +0530

    First Commit

---
+++ ARRAY/FindSecondLargest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class FindSecondLargest{
+    public static int findSecondLargest(int[] arr){
+        Arrays.sort(arr);
+        for(int i = arr.length-1;i>0;i--){
+            if(arr[i]!=arr[i-1]){
+                return arr[i-1];
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+    public static void main(String[] args) {
+        int[] arr = {9,9,9,9,9};
+        System.out.println("Second Largest Is: "+findSecondLargest(arr));
+    }
+}
